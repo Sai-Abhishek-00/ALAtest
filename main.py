c975c584168c8949c564cd27e1e00a4b71515cbf
abhishek <saiabhishek94@gmail.com>
2021-03-24 15:09:47 +0100

    Refactored code in pycharm

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+"""Not adding constraints to phone number length as it varies from country to country
+and also extention phones such as in hotel or offices have only single digit calling features"""
 import csv
+
+
 def read_operator_rate_file(filename):
     """This function reads the file with operators and their prices
     The variable filename contains the name of csv with operator list"""
@@ -7,6 +11,7 @@ def read_operator_rate_file(filename):
             return list(csv.reader(f))
     except FileNotFoundError as errorcode:
         print(errorcode)
+
 
 def get_input():
     """Function to read user input
@@ -17,11 +22,14 @@ def get_input():
     while True:
         try:
             phone_number_to_dial = input("Enter phone number>> ")
-            assert (phone_number_to_dial.startswith('+') and phone_number_to_dial[1:].isdigit()) or  phone_number_to_dial[:].isdigit(), 'Invalid phone number'
+            assert (phone_number_to_dial.startswith('+') and phone_number_to_dial[
+                                                             1:].isdigit()) or phone_number_to_dial[
+                                                                               :].isdigit(), 'Invalid phone number'
         except Exception as e:
             print(e)
         else:
-            return(phone_number_to_dial)
+            return (phone_number_to_dial)
+
 
 def get_max_extention_length(rate_list):
     """This function serves two tasks.
@@ -29,7 +37,8 @@ def get_max_extention_length(rate_list):
     2. Get the length of the maximum extention in the Rate List.
     """
     extention_codes_list = [str(extention[0]) for extention in rate_list]
-    return extention_codes_list,len(max(extention_codes_list, key=len))
+    return extention_codes_list, len(max(extention_codes_list, key=len))
+
 
 def extract_operator_extention(input_number):
     """This funstion checks if the extention entered by the user is in the Rate List or not.
@@ -48,6 +57,7 @@ def extract_operator_extention(input_number):
                 break
     return (extention_to_dial)
 
+
 def cheapest_call_rate(dialing_operator):
     """Function to get all possible operators who allow calling to a certain extention
     The allowed operators list will contain a list of lists in the format [extention, rate, operator]
@@ -60,14 +70,17 @@ def cheapest_call_rate(dialing_operator):
 
 
 if __name__ == '__main__':
-    filename = 'rate_list.csv' #variable with csv filename
-    rate_list = read_operator_rate_file(filename) # rate list has the information from csv file, it contains all the [extention, rate, operator]
-    extention_codes_list,max_extention_length = get_max_extention_length(rate_list) #extention_code_list has only the allowed extentions by operator. This is to minimize operations on list of lists
-    input_number = get_input() # store the user defined phone number for which the rate needs to be checked
-    dialing_operator = extract_operator_extention(input_number) #Store only the operator of the user entered phone number
+    filename = 'rate_list.csv'  # variable with csv filename
+    rate_list = read_operator_rate_file(
+        filename)  # rate list has the information from csv file, it contains all the [extention, rate, operator]
+    extention_codes_list, max_extention_length = get_max_extention_length(
+        rate_list)  # extention_code_list has only the allowed extentions by operator. This is to minimize operations on list of lists
+    input_number = get_input()  # store the user defined phone number for which the rate needs to be checked
+    dialing_operator = extract_operator_extention(
+        input_number)  # Store only the operator of the user entered phone number
     if dialing_operator == "Operator not found":
         print(dialing_operator)
         exit()
-    price = cheapest_call_rate(dialing_operator)[1] # Store the cheapest price for a certain extention
-    operator = cheapest_call_rate(dialing_operator)[2] # Store the operator with cheapest price for a certain extention
-    print("cheapest price is", price, "with the operator", operator) # Show the output.
+    price = cheapest_call_rate(dialing_operator)[1]  # Store the cheapest price for a certain extention
+    operator = cheapest_call_rate(dialing_operator)[2]  # Store the operator with cheapest price for a certain extention
+    print("cheapest price is", price, "with the operator", operator)  # Show the output.
